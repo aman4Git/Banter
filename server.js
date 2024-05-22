0be1587733e5cbcedf5940309535ca3f997aedcc
Aman Mishra <amanm4345@gmail.com>
2024-05-22 18:25:45 +0530

    FEAT: Install socket.io, Setup socket.io in server.js and client client.js.

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const http = require('http').createServer(app);
 
-
-
 //Start the server
 http.listen(PORT, () => {
     console.log(`Server listening on localhost:${PORT}`);
@@ -14,4 +12,11 @@ app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
+});
+
+//Socket connection
+const io = require('socket.io')(http);
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
 });
