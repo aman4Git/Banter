37332b475764c8fb6e9a5cb6e923bb590f03edd6
Aman Mishra <amanm4345@gmail.com>
2024-05-22 18:19:41 +0530

    FEAT: Static page added.

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ http.listen(PORT, () => {
     console.log(`Server listening on localhost:${PORT}`);
 });
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
